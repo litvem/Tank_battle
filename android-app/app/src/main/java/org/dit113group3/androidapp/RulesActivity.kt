28c6de2291ff2bc6de6d7f2aff80f48865c6e4cb
albaadani1996 <gusalbkh@student.gu.se>
2022-05-16 14:11:59 +0200

    Create layout for rules screen
    Related issues: #35

---
+++ android-app/app/src/main/java/org/dit113group3/androidapp/RulesActivity.kt
@@ -0,0 +1,11 @@
+package org.dit113group3.androidapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RulesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_rules)
+    }
+}
