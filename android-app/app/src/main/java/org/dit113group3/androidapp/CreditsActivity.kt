4c9a3609cd7e09263d27930ba16899a28da7db37
Operatik <flawkye@gmail.com>
2022-05-19 13:11:33 +0200

    Enable hyperlinks on activity
    Related issue(s): #36

--- android-app/app/src/main/java/org/dit113group3/androidapp/CreditsActivity.kt
+++ android-app/app/src/main/java/org/dit113group3/androidapp/CreditsActivity.kt
@@ -1,11 +1,29 @@
 package org.dit113group3.androidapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
 import android.os.Bundle
+import android.text.method.LinkMovementMethod
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class CreditsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_credits)
+        setupHyperlink()
+    }
+
+    private fun setupHyperlink() {
+        val devBodyLink = findViewById<TextView>(R.id.devBody)
+        devBodyLink.movementMethod = LinkMovementMethod.getInstance()
+        devBodyLink.setLinkTextColor(Color.BLUE)
+
+        val teacherBodyLink = findViewById<TextView>(R.id.teacherBody)
+        teacherBodyLink.movementMethod = LinkMovementMethod.getInstance()
+        teacherBodyLink.setLinkTextColor(Color.BLUE)
+
+        val taBodyLink = findViewById<TextView>(R.id.taBody)
+        taBodyLink.movementMethod = LinkMovementMethod.getInstance()
+        taBodyLink.setLinkTextColor(Color.BLUE)
     }
 }
